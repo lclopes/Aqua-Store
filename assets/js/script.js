e077a20d0f2d6165fc56a31f2c8c10b7b78d7246
Lucas Lopes <44760298+lu-dcs@users.noreply.github.com>
2019-09-13 11:10:14 -0300

    added jquery files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
+/// <reference path="../../typings/globals/jquery/index.d.ts" />
+
 $(function() {
    $('[data-toggle="tooltip"]').tooltip();
    $('[data-toggle="popover"]').popover();
+
+   $("#submitbtn").click(function(){
+       //alert("consegui");
+       $("#nome").addClass("is-invalid");
+   })
+
+
 })
+
