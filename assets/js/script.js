a319f7ad4c0538ca53f51b8854cbd9be1085d845
Lucas Lopes <44760298+lu-dcs@users.noreply.github.com>
2019-09-13 12:17:27 -0300

    coding validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
 /// <reference path="../../typings/globals/jquery/index.d.ts" />
 
-$(function() {
+$(function () {
    $('[data-toggle="tooltip"]').tooltip();
    $('[data-toggle="popover"]').popover();
+   /*
+      $("#submitbtn").click(function(){
+          //alert("consegui");
+          $("#nome").validate();   
+          $("#sexo-masc").addClass("is-invalid");
+          $("#sexo-fem").addClass("is-invalid");
+          $("#endereco").addClass("is-invalid");
+      })
+   */
 
-   $("#submitbtn").click(function(){
-       //alert("consegui");
-       $("#nome").addClass("is-invalid");
-   })
-
+   $("#cadastro").validate({
+      rules: {
+         nome: "required",
+         sexo:{
+            required: function () {
+               return $('[name="sexo"]:checked').length === 0;
+            }
+         },
+         endereco: "required",
+         
+      },
+      errorPlacement: function (error, element) {
+         if (element.is(":radio")) {
+            error.appendTo('#erro-sexo');
+         }
+         else { // This is the default behavior 
+            error.insertAfter(element);
+         }
+      },
+      submitHandler: function (form) {
+         form.submit();
+      }
+   });
 
 })
+
+
 
