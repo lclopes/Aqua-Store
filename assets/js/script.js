f4e53a7707c2dceaa8988c17720d39e4238fe680
Lucas Lopes <lucascastro@id.uff.br>
2019-09-21 00:46:59 -0300

    validating (almost) all fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 $(function () {
    $('[data-toggle="tooltip"]').tooltip();
    $('[data-toggle="popover"]').popover();
-   
+
 })
 
 /*
@@ -17,15 +17,15 @@ $(function () {
 
       }) */
 
-      /*
+/*
 $(document).ready(function(){
-   $('#form-cadastro input[type="text"]').blur(function(){
-      if(!$(this).val()){
-          $(this).addClass("is-invalid");
-      } else{
-          $(this).removeClass("is-invalid");
-      }
-  });
+$('#form-cadastro input[type="text"]').blur(function(){
+if(!$(this).val()){
+    $(this).addClass("is-invalid");
+} else{
+    $(this).removeClass("is-invalid");
+}
+});
 })*/
 
 // $(function(){   
@@ -41,17 +41,17 @@ $(document).ready(function(){
 
 //checkbox do formulário de cartão de crédito
 $(function () {
-   $("#chk-formcard").click(function(){
-      if($(this).is(":checked")){
-         $("#creditcard").prop("disabled",true);
-         $("#chk-end").prop("disabled",true);
+   $("#chk-formcard").click(function () {
+      if ($(this).is(":checked")) {
+         $("#creditcard").prop("disabled", true);
+         $("#chk-end").prop("disabled", true);
          $("#num-card").attr("disabled", "disabled");
          $("#cvv").attr("disabled", "disabled");
          $("#endereco-cob").attr("disabled", "disabled");
          $("#expiration").attr("disabled", "disabled");
       } else {
-         $("#creditcard").prop("disabled",false);
-         $("#chk-end").prop("disabled",false);
+         $("#creditcard").prop("disabled", false);
+         $("#chk-end").prop("disabled", false);
          $("#num-card").removeAttr("disabled");
          $("#cvv").removeAttr("disabled");
          $("#endereco-cob").removeAttr("disabled");
@@ -62,8 +62,8 @@ $(function () {
 
 //checkbox de endereço de cobrança
 $(function () {
-   $("#chk-end").click(function(){
-      if($(this).is(":checked")){
+   $("#chk-end").click(function () {
+      if ($("#chk-end").is(":checked")) {
          $("#endereco-cob").attr("disabled", "disabled");
       } else {
          $("#endereco-cob").removeAttr("disabled");
@@ -74,36 +74,92 @@ $(function () {
 
 //ESSE PEDAÇO ESTÁ VALIDANDO ESSES CAMPOS AO CLICAR EM ENVIAR
 //FALTA MELHORAR UM POUCO ESSE CÓDIGO, MAS A VALIDAÇÃO ATÉ QUE FUNCIONA
-$('#submitbtn').on('click',function()
-{
-   if($('#nome').val()) {
+$('#submitbtn').on('click', function () {
+   if ($('#nome').val()) {
       $('#nome').removeClass("is-invalid");
    }
-   if(!$('#nome').val()) {
+   if (!$('#nome').val()) {
       $('#nome').addClass("is-invalid");
       $('#nome').focus();
    }
-   if($('#email').val()) {
+   if ($('#email').val()) {
       $('#email').removeClass("is-invalid");
    }
-   if(!$('#email').val()) {
+   if (!$('#email').val()) {
       $('#email').addClass("is-invalid");
       $('#email').focus();
    }
-   if($('#phone').val()) {
+   if ($('#phone').val()) {
       $('#phone').removeClass("is-invalid");
    }
-   if(!$('#phone').val()) {
+   if (!$('#phone').val()) {
       $('#phone').addClass("is-invalid");
       $('#phone').focus();
    }
 
-   if($('#endereco').val()) {
+   if(!$("#sexo").is(":checked")){
+      $('#sexo-masc').addClass("is-invalid");
+      $('#sexo-fem').addClass("is-invalid");
+      $('#sexo').addClass("is-invalid");
+   }   
+   if($("#sexo").is(":checked")){
+      $('#sexo-masc').removeClass("is-invalid");
+      $('#sexo-fem').removeClass("is-invalid");
+      $('#sexo').removeClass("is-invalid");
+   }
+   
+
+   if(!$("#creditcard").find(":selected").val() == -1){
+      $('#creditcard').addClass("is-invalid");
+   }   
+
+
+   if ($('#endereco').val()) {
       $('#endereco').removeClass("is-invalid");
    }
-   if(!$('#endereco').val()) {
+   if (!$('#endereco').val()) {
       $('#endereco').addClass("is-invalid");
       $('#endereco').focus();
+   }
+   if (!$('#num-card').val() && !$("#chk-formcard").is(":checked")) {
+      $('#num-card').addClass("is-invalid");
+      $('#num-card').focus();
+   }
+
+   if ($('#num-card').val() && !$("#chk-formcard").is(":checked")) {
+      $('#num-card').removeClass("is-invalid");
+   }
+
+   if (!$('#cvv').val() && !$("#chk-formcard").is(":checked")) {
+      $('#cvv').addClass("is-invalid");
+      $('#cvv').focus();
+   }
+
+   if ($('#cvv').val() && !$("#chk-formcard").is(":checked")) {
+      $('#cvv').removeClass("is-invalid");
+   }
+
+   if (!$('#expiration').val() && !$("#chk-formcard").is(":checked")) {
+      $('#expiration').addClass("is-invalid");
+      $('#expiration').focus();
+   }
+
+   if ($('#expiration').val() && !$("#chk-formcard").is(":checked")) {
+      $('#expiration').removeClass("is-invalid");
+   }
+
+   if (!$('#endereco-cob').val() && !$("#chk-formcard").is(":checked")) {
+      if (!$("#chk-end").is(":checked")) {
+         $('#endereco-cob').addClass("is-invalid");
+         $('#endereco-cob').focus();
+      }
+
+   }
+
+   if ($('#endereco-cob').val() && !$("#chk-formcard").is(":checked")) {
+      if (!$("#chk-end").is(":checked")) {
+         $('#endereco-cob').removeClass("is-invalid");
+      }
    }
 });
 
