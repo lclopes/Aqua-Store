1b9f57925510a7f12342cb1b03f2718ccb32ee3a
Lucas Lopes <lucascastro@id.uff.br>
2019-09-27 13:10:48 -0300

    Fixed layout for like/dislike buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,11 +26,28 @@ $(function () {
          var input = $(this).find('.qtd1');
          input.val(parseInt(input.val()) + 1);
          $("#like").data("like",input);
-         
+
       } else if ($("#like").hasClass("fas fa-thumbs-up")) {
          $("#like").removeClass("fas fa-thumbs-up");
          $("#like").addClass("far fa-thumbs-up");
          var input = $(this).find('.qtd1');
+         input.val(parseInt(input.val()) - 1);
+      }
+   });
+
+   $("#data-dislike").click(function () {
+      if ($("#dislike").hasClass("far fa-thumbs-down")) {
+         $("#dislike").removeClass("far fa-thumbs-down");
+         $("#dislike").addClass("fas fa-thumbs-down");
+         
+         var input = $(this).find('.qtd2');
+         input.val(parseInt(input.val()) + 1);
+         $("#dislike").data("dislike",input);
+         
+      } else if ($("#dislike").hasClass("fas fa-thumbs-down")) {
+         $("#dislike").removeClass("fas fa-thumbs-down");
+         $("#dislike").addClass("far fa-thumbs-down");
+         var input = $(this).find('.qtd2');
          input.val(parseInt(input.val()) - 1);
       }
    });
